55a2b985c153d212b3c46ddad3f09d1e9e881244
Aptalk116 <89452474+Aptalk116@users.noreply.github.com>
2022-05-30 16:47:03 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,3 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-
-workbox.routing.registerRoute(
-   ({request}) => request.destination === 'image',
-   new.workbox.strategies.NetworkFirst()
-);
-
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open('app-store').then((cache) => cache.addAll([
